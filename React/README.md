01eac9a4f27f617065bb99ff145dd6d7b44a4d8c
GauravBhandari19 <gaurav.bhandari85@gmail.com>
2020-04-05 03:29:40 +0100

    1

--- React/README.md
+++ React/README.md
@@ -4,3 +4,13 @@
 ```
 npx create-react-app simple-calculator
 ```
+
+## To use scss, install it
+```
+npm install node-sass
+```
+## Install Bootsrap
+```
+npm install react-bootstrap bootstrap
+```
+
---
+++ React/simple-calculator/src/components/header.component.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+//import {Link} from 'react-router-dom';
+//import Button from 'react-bootstrap/Button';
+
+export default class Header extends Component {
+
+render()
+{
+    return(
+        <table>
+             <thead>
+    <tr>
+      <th>#</th>
+      <th>Table heading</th>
+      <th>Table heading</th>
+      <th>Table heading</th>
+      <th>Table heading</th>
+      <th>Table heading</th>
+      <th>Table heading</th>
+    </tr>
+  </thead>
+        </table>
+    )
+}
+}
