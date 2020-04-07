51f8c8c97ae08ce87effaa4034d45a7e0def8b14
GauravBhandari19 <gaurav.bhandari85@gmail.com>
2020-04-07 03:09:42 +0100

    Center Aligned the calculator

--- React/simple-calculator/src/App.js
+++ React/simple-calculator/src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 //import Header from './components/header.component'
 import Calculator from './components/Calculator.component';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="container border">
-      <Calculator></Calculator>
+    <Container>
+    <div className="row text-center">
+      <div className="col-md"><Calculator /></div>
     </div>
+    </Container>
   );
-}
+  }
+
+  function App_bk() {
+    return (
+      <Container>
+      <div className="row text-center">
+        <div className="col-md">.col</div>
+      </div>
+      </Container>
+    );
+    }
 
 export default App;
